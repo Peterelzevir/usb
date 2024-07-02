c9eaf0454ee9a285ffa1b815178875eee8e5177b
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2024-07-02 15:28:14 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
 api_id = '28356794'
 api_hash = 'a581331dabc5d4b7e0c7381a97dde824'
-main_admin_id = '5988451717'
+main_admin_username = 'hiyaok'  # Ganti dengan username admin utama Anda
