fe0405ea2182fc6f0d413dc685c595536d520dea
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2024-07-03 20:02:42 +0700

    Update index.py

--- index.py
+++ index.py
@@ -4,8 +4,7 @@ import os
 from telethon import TelegramClient, events
 from telethon.tl.types import MessageMediaPhoto, MessageMediaDocument
 from telethon.tl.types import InputMessageEntityMentionName
-from telethon.tl.types import MessageEntity
-from telethon.tl.types import MessageEntityTextUrl
+from telethon.tl.entities import MessageEntity
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError, PhoneCodeInvalidError, FloodWaitError
 
 # Konfigurasi API Telegram
